24cbfe5e87181ddf8c5515892ede6933fa12c1dd
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-07-16 21:27:41 -0500

    Finished css and footer for landing page. Input footer and information as well.

--- light/static/light/js/script.js
+++ light/static/light/js/script.js
@@ -13,16 +13,5 @@ $(document).ready(function() {
              //Stuff to do *after* the animation takes place
          })
      })
-     $('a.new_review').click(function(){
-         $('div.all_reviews').slideUp('fast', function() {
-             $('#review_form').show('slow')
-             //Stuff to do *after* the animation takes place
-         })
-     })
-     $('a.cancel').click(function(){
-         $('#review_form').hide('fast')
-         $('.all_reviews').slideDown('slow', function() {
-             //Stuff to do *after* the animation takes place
-         })
-     })
+    
 });

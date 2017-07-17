2fe2816346dbe2f5fd67bfe6821770dad3071410
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-07-17 17:11:31 -0500

    Finished styles for index page. Added jQuery animation for login and registration.

--- light/static/light/js/script.js
+++ light/static/light/js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
      // Stuff to do as soon as the DOM is ready
-     $('h3#enter_button').click(function(){
-         $('.index_container').slideUp('slow', function() {
-             $('div.reg_and_login_container').slideDown('slow', function() {
-             })
-         });
-     });
-     $('h1#cancel_button').click(function(){
-         $('.reg_and_login_container').slideUp('slow', function() {
-         })
-         $('.index_container').slideDown('slow', function() {
-             //Stuff to do *after* the animation takes place
-         })
+     $('#my_popup').popup();
+     $('ul.tabs').tabs();
+     $('#my_popup').on('click', '.login_btn', function() {
+         $('.register').hide();
+         $('.login').show()
      })
-    
+     $('#my_popup').on('click', '.register_btn', function() {
+         $('.register').show();
+         $('.login').hide()
+     })
+    //  $('h1#cancel_button').click(function(){
+    //      $('.reg_and_login_container').slideUp('slow', function() {
+    //      })
+    //      $('.index_container').slideDown('slow', function() {
+    //          //Stuff to do *after* the animation takes place
+    //      })
+    //  })
+
 });
